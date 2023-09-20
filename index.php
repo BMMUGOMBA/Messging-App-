9bc6b1dee2cdf545d268ff5bba797d92667b9ffb
ProfBatsi <bmmugomba@gmail.com>
2023-09-20 03:12:51 +0200

    index flash data

---
+++ index.php
@@ -0,0 +1,73 @@
+<!--
+    php code to test is session is set and redirect to login if not set
+-->
+<?php
+session_start();
+if(!isset($_SESSION['id'])){
+    header("Location:./login.php");
+    exit;
+}
+//else get database connection
+require_once('./DBConnection.php');
+//set sesssion variables
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo ucwords(str_replace('_',' ',$page)) ?> |Messaging Web Application || ChatApp</title>
+    <link rel="stylesheet" href="./fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/custom.css">
+    <script src="./js/jquery-3.6.0.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./fontawesome/js/all.min.js"></script>
+</head>
+<body class="bg-light">
+<main>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient" id="topNavBar">
+//navigation bar bootstrap
+        <div class="container">
+            <a class="navbar-brand" href="./">
+           Messaging Web Application 
+            </a>
+            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button> -->
+            <!-- <div class="collapse navbar-collapse" id="navbarNav">
+                
+            </div> -->
+            <div>
+            <?php if(isset($_SESSION['id'])): ?>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle bg-transparent  text-light border-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="me-1"><img src="<?php echo is_file('./uploads/avatars/'.$_SESSION['id'].'.png') ? './uploads/avatars/'.$_SESSION['id'].'.png?v='.(is_null($_SESSION['date_updated']) ? strtotime($_SESSION['date_created']) : strtotime($_SESSION['date_updated'])) : './images/no-image-available.png' ?>" alt="avatar" class="avatar rounded-circle"></span>    Hello <?php echo $_SESSION['firstname'] ?>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="./?page=profile">My Profile</a></li>
+                        <li><a class="dropdown-item" href="./Actions.php?a=logout">Logout</a></li>
+                    </ul>
+                </div>
+            <?php endif; ?>
+            </div>
+        </div>
+    </nav>
+    //gets and displays info by using including page.php
+    <div class="container py-3" id="page-container">
+        <?php 
+            if(isset($_SESSION['flashdata'])):
+        ?>
+        <div class="dynamic_alert alert alert-<?php echo $_SESSION['flashdata']['type'] ?>">
+        <div class="float-end"><a href="javascript:void(0)" class="text-dark text-decoration-none" onclick="$(this).closest('.dynamic_alert').hide('slow').remove()">x</a></div>
+            <?php echo $_SESSION['flashdata']['msg'] ?>
+        </div>
+        <?php unset($_SESSION['flashdata']) ?>
+        <?php endif; ?>
+        <?php
+            include $page.'.php';
+        ?>
+    </div>
